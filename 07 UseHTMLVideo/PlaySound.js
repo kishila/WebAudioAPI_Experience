d7ec87d33bba3033573bd350ebeee754529a85ad
kishila <s16g459@stu.kagawa-u.ac.jp>
2016-04-11 06:07:39 +0900

    videタグを入力点に

---
+++ 07 UseHTMLVideo/PlaySound.js
@@ -0,0 +1,85 @@
+window.onload = function(){
+  window.AudioContext = window.AudioContext || window.webkitAudioContext;
+  var animationId;
+  var audioContext = new AudioContext;
+  var oscillator = null;
+  var audio;
+  var isStop = true;
+
+  // ゲインノードの構築
+  audioContext.createGain = audioContext.createGain || audioContext.createGainNode;
+  var gain = audioContext.createGain();
+
+  // アナライザノードの構築
+  var analyser = audioContext.createAnalyser();
+  analyser.fftSize = 128; // 高速フーリエ変換の分割サイズ
+
+  // キャンバスの構築
+  var canvas        = document.getElementById('visualizer');
+  var canvasContext = canvas.getContext('2d');
+  canvas.setAttribute('width', analyser.frequencyBinCount * 10);
+
+  document.getElementById('sound_button').addEventListener('click', function() {
+    if(isStop) {
+      video = document.getElementById("video");
+
+      video.addEventListener('play', function(event) {
+        // Create the instance of MediaElementAudioSourceNode
+        var source = audioContext.createMediaElementSource(video);
+
+        // AudioBufferSourceNode (Input) -> GainNode (Volume) -> Analyser(Visualizer) -> AudioDestinationNode (Output)
+        source.connect(gain);
+        gain.connect(analyser);
+        analyser.connect(audioContext.destination);
+
+        // Set parameters
+        video.playbackRate = document.getElementById('range-playback-rate').valueAsNumber;
+      });
+
+      // Start audio
+      video.play();
+
+      // Animation
+      animationId = requestAnimationFrame(render);
+
+      isStop = false;
+      document.getElementById('sound_icon').classList.remove("icon-start");
+      document.getElementById('sound_icon').classList.add("icon-stop");
+    } else {
+      video.pause();
+      video = null;
+      cancelAnimationFrame(animationId)
+      isStop = true;
+      document.getElementById('sound_icon').classList.remove("icon-stop");
+      document.getElementById('sound_icon').classList.add("icon-start");
+    }
+  });
+
+  render = function(){
+    var spectrums = new Uint8Array(analyser.frequencyBinCount);
+    analyser.getByteFrequencyData(spectrums);
+　
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+    for(var i=0, len=spectrums.length; i<len; i++){
+      canvasContext.fillRect(i*10, 0, 5, spectrums[i]);
+    }
+
+    animationId = requestAnimationFrame(render);
+  };
+
+  document.getElementById('range-volume').addEventListener('input', function() {
+    var min = gain.gain.minValue || 0;
+    var max = gain.gain.maxValue || 1;
+    if ((this.valueAsNumber >= min) && (this.valueAsNumber <= max)) {
+      gain.gain.value = this.valueAsNumber;
+      document.getElementById('output-volume').textContent = this.value;
+    }
+  });
+
+  document.getElementById('range-playback-rate').addEventListener('input', function() {
+    if(video){
+      video.playbackRate = this.valueAsNumber;
+    }
+    document.getElementById('playback-rate').textContent = this.value;
+  });
+};
---
+++ 07 UseHTMLVideo/README.md
@@ -0,0 +1,6 @@
+## About this project
+
+```txt
+HTMLに埋め込まれたAudioタグを入力点とし、音を再生
+playbackRateはChromeは対応しているがfirefoxは対応していない (2016/04/04)
+```
