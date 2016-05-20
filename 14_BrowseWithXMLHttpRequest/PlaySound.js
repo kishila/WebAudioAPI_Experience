a9ffbc817dd0cf0f0ef3ac6b2c6753f6312ac5b2
kishila <s16g459@stu.kagawa-u.ac.jp>
2016-05-21 04:48:13 +0900

    サーバー側のファイルを参照

---
+++ 14_BrowseWithXMLHttpRequest/PlaySound.js
@@ -0,0 +1,41 @@
+window.onload = function(){
+  window.AudioContext = window.AudioContext || window.webkitAudioContext;
+  var audioContext = new AudioContext;
+  var oscillator = null;
+  var isStop = true;
+  var seBuffer;
+
+  var xhr = new XMLHttpRequest();
+  var url = './mp3/se.mp3';
+
+  xhr.onload = function() {
+      if (xhr.status === 200) {
+          var arrayBuffer = xhr.response;
+          if (arrayBuffer instanceof ArrayBuffer) {
+              var successCallback = function(audioBuffer) {
+                seBuffer = audioBuffer;
+              };
+
+              var errorCallback = function(error) {
+                  if (error instanceof Error) {
+                      window.alert(error.message);
+                  } else {
+                      window.alert('Error : "decodeAudioData" method.');
+                  }
+              };
+              audioContext.decodeAudioData(arrayBuffer, successCallback, errorCallback);
+          }
+      }
+  };
+
+  xhr.open('GET', url, true);
+  xhr.responseType = 'arraybuffer';  // XMLHttpRequest Level 2
+  xhr.send(null);
+
+  document.getElementById('sound_button').addEventListener('click', function(){
+    var source = audioContext.createBufferSource();
+    source.buffer = seBuffer;
+    source.connect(audioContext.destination);
+    source.start(0);
+  });
+}
